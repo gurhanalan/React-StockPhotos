cac35b757b596ac09e5de47e0f32425d3552049b
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-02-08 02:25:18 +0300

    Add Files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+// https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY_UNSPLASH}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+    const [loading, setLoading] = useState(false);
+    const [photos, setPhotos] = useState([]);
+
+    const fetchImages = async () => {
+        setLoading(true);
+        let url;
+        url = `${mainUrl}${clientID}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            // console.log(data);
+            setPhotos(data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchImages();
+    }, []);
+    return <h2>stock photos starter</h2>;
+}
+
+export default App;
