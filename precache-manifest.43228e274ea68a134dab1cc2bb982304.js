13f7bae8e582b16b70fd1d5b5f118bd036721eb1
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-06-25 19:44:37 +0300

    Updates

---
+++ precache-manifest.43228e274ea68a134dab1cc2bb982304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb06d012dae9913e0512ab774d997a2",
+    "url": "/React-StockPhotos/index.html"
+  },
+  {
+    "revision": "b21886f13e6292b1170a",
+    "url": "/React-StockPhotos/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "684df2c19a08ba64f4e4",
+    "url": "/React-StockPhotos/static/js/2.8e28d41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-StockPhotos/static/js/2.8e28d41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21886f13e6292b1170a",
+    "url": "/React-StockPhotos/static/js/main.3a9b51c1.chunk.js"
+  },
+  {
+    "revision": "8ec82b4ca25319c712ce",
+    "url": "/React-StockPhotos/static/js/runtime-main.64fca732.js"
+  }
+]);
